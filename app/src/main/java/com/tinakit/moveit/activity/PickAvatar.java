b3a6eb3f2930adb734bcf489e41e204db5994e43
Tina Fredericks <tina.k.fredericks@gmail.com>
2016-01-10 13:47:05 -0800

    convert PickAvatar.java to Fragment

--- app/src/main/java/com/tinakit/moveit/activity/PickAvatar.java
+++ app/src/main/java/com/tinakit/moveit/activity/PickAvatar.java
@@ -2,11 +2,15 @@ package com.tinakit.moveit.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.tinakit.moveit.R;
 import com.tinakit.moveit.adapter.PickAvatarRecyclerAdapter;
@@ -23,7 +27,7 @@ import javax.inject.Inject;
 /**
  * Created by Tina on 1/1/2016.
  */
-public class PickAvatar extends AppCompatActivity {
+public class PickAvatar extends Fragment {
 
     // CONSTANTS
     public static final String PICK_AVATAR_TAG = "PICK_AVATAR";
@@ -31,8 +35,9 @@ public class PickAvatar extends AppCompatActivity {
     public static final int PICK_AVATAR_REQUEST_CODE = 1;
 
     // local cache
-    protected static List<Reward> mRewardList;
     protected FragmentActivity mFragmentActivity;
+    private View rootView;
+    protected static List<Reward> mRewardList;
     protected User mUser;
 
     // UI COMPONENTS
@@ -43,29 +48,25 @@ public class PickAvatar extends AppCompatActivity {
     FitnessDBHelper mDatabaseHelper;
 
 
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.recycler_view);
-        setRequestedOrientation(android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        // inject FitnessDBHelper
-        ((CustomApplication)getApplication()).getAppComponent().inject(this);
-
-        //get databaseHelper instance
-        //mDatabaseHelper = FitnessDBHelper.getInstance(mFragmentActivity);
+        mFragmentActivity  = (FragmentActivity)super.getActivity();
+        rootView = inflater.inflate(R.layout.recycler_view, container, false);
+        mFragmentActivity.setRequestedOrientation(android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         // fetch data before initializing UI
         fetchData();
 
         initializeUI();
 
-
+        return rootView;
     }
 
     private void fetchData(){
 
-        Bundle bundle = getIntent().getExtras();
+        Bundle bundle = getActivity().getIntent().getExtras();
         if (bundle != null && bundle.containsKey(PICK_AVATAR_KEY_USER)){
 
             mUser = bundle.getParcelable(PICK_AVATAR_KEY_USER);
@@ -78,13 +79,13 @@ public class PickAvatar extends AppCompatActivity {
 
         //RecyclerView
         // Initialize recycler view
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
         //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mFragmentActivity);
         //linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         //mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
 
 
@@ -92,7 +93,7 @@ public class PickAvatar extends AppCompatActivity {
         // get array list of avatar filenames from string array resource
         List<String> avatarFileList = Arrays.asList(getResources().getStringArray(R.array.avatar_images));
 
-        mPickAvatarRecyclerAdapter = new PickAvatarRecyclerAdapter(getApplicationContext(), this, avatarFileList, mUser);
+        mPickAvatarRecyclerAdapter = new PickAvatarRecyclerAdapter(getActivity(), mFragmentActivity, avatarFileList, mUser);
         mRecyclerView.setAdapter(mPickAvatarRecyclerAdapter);
     }
 }
