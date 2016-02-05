b54758c7f2c09915859f0255d6c14ca3f6fa41a9
Tina Fredericks <tina.k.fredericks@gmail.com>
2016-02-05 11:06:28 -0800

    LocationApi - POLLING_FREQUENCY = 2 * 1000 LOCATION_ACCURACY = 4

--- app/src/main/java/com/tinakit/moveit/api/LocationApi.java
+++ app/src/main/java/com/tinakit/moveit/api/LocationApi.java
@@ -34,9 +34,9 @@ public class LocationApi implements LocationListener, LifeCycle {
     //LocationRequest settings
     private LocationRequest mLocationRequest;
     private Location mBestReading;
-    private static long POLLING_FREQUENCY = 5 * 1000; // in milliseconds
+    private static long POLLING_FREQUENCY = 2 * 1000; // in milliseconds, standard is 5 seconds
     private static long FASTEST_POLLING_FREQUENCY = 5 * 1000; // in milliseconds
-    private static final long LOCATION_ACCURACY = 10; //within # meter accuracy
+    private static final long LOCATION_ACCURACY = 4; //within # meter accuracy, standard is 20 meters
     private boolean mIsTimeLimit = false;
 
 
