bef3b6f727466c93f053ee7078b7cd7a1c05b17b
Tina Fredericks <tina.k.fredericks@gmail.com>
2016-02-29 11:05:38 -0800

    PickAvatar - delete extra spacing

--- app/src/main/java/com/tinakit/moveit/activity/PickAvatar.java
+++ app/src/main/java/com/tinakit/moveit/activity/PickAvatar.java
@@ -48,38 +48,26 @@ public class PickAvatar extends AppCompatActivity {
     @Inject
     FitnessDBHelper mDatabaseHelper;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pick_avatar);
         setRequestedOrientation(android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         // inject FitnessDBHelper
         ((CustomApplication)getApplication()).getAppComponent().inject(this);
-
         // fetch data before initializing UI
         fetchData();
-
         initializeUI();
-
     }
 
     private void fetchData(){
-
         Bundle bundle = getIntent().getExtras();
-        if (bundle != null && bundle.containsKey(PICK_AVATAR_KEY_USER)){
-
-            mUser = bundle.getParcelable(PICK_AVATAR_KEY_USER);
-
+        if (bundle != null && bundle.containsKey(PICK_AVATAR_KEY_USER)) {
+             mUser = bundle.getParcelable(PICK_AVATAR_KEY_USER);
         }
-
     }
 
     private void initializeUI(){
-
-        //RecyclerView
-        // Initialize recycler view
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
@@ -88,11 +76,9 @@ public class PickAvatar extends AppCompatActivity {
         SpannableString content = new SpannableString(getResources().getString(R.string.sachan_link));
         content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
         attribution.setText(content);
-
         attribution.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://thenounproject.com/sachan/collection/animals/?oq=animal%20sachan&;cidx=8"));
                 startActivity(browserIntent);
             }
@@ -100,7 +86,6 @@ public class PickAvatar extends AppCompatActivity {
 
         // get array list of avatar filenames from string array resource
         List<String> avatarFileList = Arrays.asList(getResources().getStringArray(R.array.avatar_images));
-
         mPickAvatarRecyclerAdapter = new PickAvatarRecyclerAdapter(getApplicationContext(), this, avatarFileList, mUser);
         mRecyclerView.setAdapter(mPickAvatarRecyclerAdapter);
     }
